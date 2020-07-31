7d41b1307502d241338a346e0b7ca85d36eb840c
iwansafr <iwansafr@gmail.com>
2020-08-01 03:27:07 +0700

    sistem

--- application/modules/admin/views/templates/AdminLTE/index.php
+++ application/modules/admin/views/templates/AdminLTE/index.php
@@ -36,7 +36,7 @@
     <div class="pull-right hidden-xs">
       <b>Version</b> 2.4.0
     </div>
-    <strong>Copyright &copy; 2015-<?php echo date('Y'); ?> <a href="https://www.esoftgreat.com">esoftgreat</a>.</strong> All rights
+    <strong>Copyright &copy; <?php echo $site['site']['year'].'-'.date('Y'); ?> <a href="<?php echo $site['site']['link'] ?>"><?php echo $site['site']['title'] ?></a>.</strong> All rights
     reserved.
   </footer>
 
