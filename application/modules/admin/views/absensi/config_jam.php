faa26c896b4adcb36a3647be53ee0306876b8a52
iwansafr <iwansafr@gmail.com>
2021-07-29 15:06:15 +0700

    config jam privillege

--- application/modules/admin/views/absensi/config_jam.php
+++ application/modules/admin/views/absensi/config_jam.php
@@ -6,31 +6,32 @@ $form->setTable('jam_absen');
 if(!empty($instansi_id))
 {
 	$form->setParamName('config_jam_instansi_'.$instansi_id);
-}else{
+}else if(is_admin() || is_root()){
 	$form->setParamName('config_jam');
+	foreach ($hari as $key => $value) 
+	{
+		$form->addInput('mulai_berangkat_'.$key,'text');
+		$form->setLabel('mulai_berangkat_'.$key,'mulai_berangkat');
+		$form->setType('mulai_berangkat_'.$key,'time');
+
+		$form->addInput('akhir_berangkat_'.$key,'text');
+		$form->setLabel('akhir_berangkat_'.$key,'akhir_berangkat');
+		$form->setType('akhir_berangkat_'.$key,'time');
+
+
+		$form->addInput('mulai_pulang_'.$key,'text');
+		$form->setLabel('mulai_pulang_'.$key,'mulai_pulang');
+		$form->setType('mulai_pulang_'.$key,'time');
+
+		$form->addInput('akhir_pulang_'.$key,'text');
+		$form->setLabel('akhir_pulang_'.$key,'akhir_pulang');
+		$form->setType('akhir_pulang_'.$key,'time');
+
+		$form->startCollapse('mulai_berangkat_'.$key, $value);
+		$form->endCollapse('akhir_pulang_'.$key);
+		$form->setCollapse('mulai_berangkat_'.$key, $value);
+	}
+	$form->form();
+}else{
+	msg('tidak punya hak ke halaman ini','danger');
 }
-
-foreach ($hari as $key => $value) 
-{
-	$form->addInput('mulai_berangkat_'.$key,'text');
-	$form->setLabel('mulai_berangkat_'.$key,'mulai_berangkat');
-	$form->setType('mulai_berangkat_'.$key,'time');
-
-	$form->addInput('akhir_berangkat_'.$key,'text');
-	$form->setLabel('akhir_berangkat_'.$key,'akhir_berangkat');
-	$form->setType('akhir_berangkat_'.$key,'time');
-
-
-	$form->addInput('mulai_pulang_'.$key,'text');
-	$form->setLabel('mulai_pulang_'.$key,'mulai_pulang');
-	$form->setType('mulai_pulang_'.$key,'time');
-
-	$form->addInput('akhir_pulang_'.$key,'text');
-	$form->setLabel('akhir_pulang_'.$key,'akhir_pulang');
-	$form->setType('akhir_pulang_'.$key,'time');
-
-	$form->startCollapse('mulai_berangkat_'.$key, $value);
-	$form->endCollapse('akhir_pulang_'.$key);
-	$form->setCollapse('mulai_berangkat_'.$key, $value);
-}
-$form->form();
