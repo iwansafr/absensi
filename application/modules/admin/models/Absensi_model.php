a50e40c7f3ab8bbdfbdcf03e6f703ae1d9e75d2d
iwansafr <iwansafr@gmail.com>
2021-07-30 15:23:51 +0700

    test

--- application/modules/admin/models/Absensi_model.php
+++ application/modules/admin/models/Absensi_model.php
@@ -16,6 +16,7 @@ class Absensi_model extends CI_Model
 		$user = $this->session->userdata(base_url('_logged_in'));
 		$sql = '';
 		$status = [];
+		$status['last_query'] = [];
 		$status['last_query'][] = 'coba';
 		$instansi_id = 0;
 		if(strtolower($user['role']) == 'karyawan'){
