d4b638518993ec2189a0b68b91e6880092556414
iwansafr <iwansafr@gmail.com>
2021-12-12 20:40:43 +0700

    code tidak valid

--- application/modules/admin/views/absensi/qr.php
+++ application/modules/admin/views/absensi/qr.php
@@ -18,9 +18,28 @@
         </div> -->
     <script type="text/javascript">
       function onScanSuccess(qrCodeMessage) {
-        document.getElementById("result").innerHTML =
+        // document.getElementById("result").innerHTML =
           // '<span class="result">' + qrCodeMessage + "</span>";
-          alert('berhasil di scan '+qrCodeMessage)
+          var xhr = new XMLHttpRequest();
+          var url = _URL+"admin/absensi/get_code";
+          xhr.onreadystatechange = function(){
+              if(this.readyState == 4 && this.status == 200){
+                let get_code = JSON.parse(this.responseText);
+                let scan_code = qrCodeMessage;
+                console.log(get_code);
+                console.log(get_code.code);
+                if(get_code.status){
+                  if(get_code.code == scan_code){
+                    alert('absnsi berhasil');
+                  }else{
+                    alert('code tidak valid');
+                  }
+                }
+              }
+          };
+          xhr.open("GET", url, true);
+          xhr.send();
+          // alert('berhasil di scan '+qrCodeMessage)
       }
       function onScanError(errorMessage) {
         //handle scan error
