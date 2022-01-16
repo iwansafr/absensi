642fce21a68f31cbfc6cd3a2d27dcab696020ac8
iwansafr <iwansafr@gmail.com>
2022-01-16 10:31:34 +0700

    try to update selisih waktu on save

--- application/modules/admin/views/absensi/edit.php
+++ application/modules/admin/views/absensi/edit.php
@@ -1,11 +1,17 @@
 <?php
-
+if(!empty($_POST))
+{
+  
+}
+$id = $this->input->get('id');
 $instansi_id = $this->pengguna_model->get_instansi_id(
   $this->session->userdata(base_url('_logged_in'))['id']
 );
 $form = new Zea();
 $form->setTable('absensi');
 $form->init('edit');
+
+$form->setId($id);
 
 $form->addInput('instansi_id','static');
 $form->setValue('instansi_id', $instansi_id);
@@ -17,7 +23,7 @@ $form->addInput('latitude','static');
 $form->setValue('latitude',0);
 
 $form->addInput('valid','static');
-$form->setValue('valid',1);
+$form->setValue('valid',0);
 
 $form->addInput('karyawan_id','dropdown');
 $form->setLabel('karyawan_id','Nama');
@@ -33,9 +39,18 @@ $form->setType('waktu','datetime-local');
 $form->addInput('jam_jadwal','text');
 $form->setType('jam_jadwal','time');
 
-$form->addInput('selisih_waktu','text');
-$form->setType('selisih_waktu','number');
+$form->addInput('selisih_waktu','static');
+$form->setValue('selisih_waktu',0);
 
 $form->setRequired('All');
 
 $form->form();
+if(!empty($_POST))
+{
+  $last_id    = !empty($id) ? $id : $form->get_insert_id();
+  $waktu      = date('H:i', strtotime($_POST['waktu']));
+  $jam_jadwal = strtotime($_POST['jam_jadwal']);
+  $selisih    = $waktu - $jam_jadwal;
+  $selisih    = $selisih / 60;
+  $form->set_data('absensi',$last_id, ['selisih_wakut'=>$selisih]);
+}
