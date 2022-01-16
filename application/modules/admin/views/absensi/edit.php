e03168fd951391f1916d135c9fb4bd2c6ccade14
iwansafr <iwansafr@gmail.com>
2022-01-16 10:13:31 +0700

    testing form absensi submit

--- application/modules/admin/views/absensi/edit.php
+++ application/modules/admin/views/absensi/edit.php
@@ -7,6 +7,18 @@ $form = new Zea();
 $form->setTable('absensi');
 $form->init('edit');
 
+$form->addInput('instansi_id','static');
+$form->setValue('instansi_id', $instansi_id);
+
+$form->addInput('longitude','static');
+$form->setValue('longitude',0);
+
+$form->addInput('latitude','static');
+$form->setValue('latitude',0);
+
+$form->addInput('valid','static');
+$form->setValue('valid',1);
+
 $form->addInput('karyawan_id','dropdown');
 $form->setLabel('karyawan_id','Nama');
 $form->tableOptions('karyawan_id','karyawan','id','nama','instansi_id = '.$instansi_id);
@@ -17,5 +29,13 @@ $form->setOptions('status',['1'=>'Berangkat','2'=>'Izin','3'=>'Terlambat','4'=>'
 $form->addInput('waktu','text');
 $form->setLabel('waktu','Waktu Absen');
 $form->setType('waktu','datetime-local');
+
+$form->addInput('jam_jadwal','text');
+$form->setType('jam_jadwal','time');
+
+$form->addInput('selisih_waktu','text');
+$form->setType('selisih_waktu','number');
+
+$form->setRequired('All');
 
 $form->form();
