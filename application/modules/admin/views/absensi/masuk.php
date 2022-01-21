8cc7437c782e01026367efe30a47e2b5f50d3bf8
iwansafr <iwansafr@gmail.com>
2022-01-21 20:40:58 +0700

    show date

--- application/modules/admin/views/absensi/masuk.php
+++ application/modules/admin/views/absensi/masuk.php
@@ -91,7 +91,7 @@
         <div class="modal-header">
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">×</span></button>
-          <h4 class="modal-title">Jadwal Hari Ini</h4>
+          <h4 class="modal-title">Jadwal Hari Ini <?php echo date('d-m-Y') ?></h4>
         </div>
         <div class="modal-body">
           <table class="table table-hovered">
