41b6593ab7948df1048f4af3bbce713ec7996563
iwansafr <iwansafr@gmail.com>
2022-06-30 15:14:08 +0700

    rekap

--- application/modules/admin/views/absensi/rekap.php
+++ application/modules/admin/views/absensi/rekap.php
@@ -3,6 +3,7 @@ if(!empty($data))
 {
 	$start_date = array_key_first($data);
 	$end_date = array_key_last($data);
+	$today = date('Y-m-d');
 	?>
 	<div class="box">
 		<div class="box-header">
@@ -115,7 +116,11 @@ if(!empty($data))
 										<?php 
 										if(in_array($value['hari'], $masuk)){
 											$total[5]++;
-											echo '<td class="bg-danger text-center" colspan="6">Absen</td>';
+											if($key <= $today ){
+												echo '<td class="bg-danger text-center" colspan="6">Absen</td>';
+											}else{
+												echo '<td class="bg-info text-center" colspan="6">Off</td>';
+											}
 										}else{
 											echo '<td class="bg-warning text-center" colspan="6">Libur</td>';
 										}
