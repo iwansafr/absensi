ec495c1c2015ef4794d1a6567d16ac46d7293e8a
esoftgreat <iwansafr@gmail.com>
2024-11-17 16:31:36 +0700

    update only admin can access limit karyawan

--- application/modules/admin/views/karyawan/limit_karyawan.php
+++ application/modules/admin/views/karyawan/limit_karyawan.php
@@ -1,25 +1,34 @@
 <?php
 
-$instansi_id = $this->input->get('s_id');
-$instansi = $this->db->query('SELECT * FROM instansi WHERE id = ?',[$instansi_id])->row_array();
-if(!empty($instansi))
-{
-  $form = new Zea();
-  $form->setHeading($instansi['nama']);
-  
-  $form->init('param');
-  $form->setTable('config');
-  $form->setParamName('limit_karyawan_'.$instansi_id);
-
-  $form->addInput('limit','text');
-  $form->setType('limit','number');
-  $form->setLabel('limit','Batas Maksimal total Karyawan');
-  $form->form();
+if(is_admin()){
+    $instansi_id = $this->input->get('s_id');
+    $instansi = $this->db->query('SELECT * FROM instansi WHERE id = ?',[$instansi_id])->row_array();
+    if(!empty($instansi))
+    {
+      $form = new Zea();
+      $form->setHeading($instansi['nama']);
+      
+      $form->init('param');
+      $form->setTable('config');
+      $form->setParamName('limit_karyawan_'.$instansi_id);
+    
+      $form->addInput('limit','text');
+      $form->setType('limit','number');
+      $form->setLabel('limit','Batas Maksimal total Karyawan');
+      $form->form();
+    }else{
+        ?>
+        <h1>Warning</h1>
+        <div class="alert alert-warning">
+            Instansi tidak ditemukan
+        </div>
+        <?php
+    }
 }else{
     ?>
     <h1>Warning</h1>
     <div class="alert alert-warning">
-        Instansi tidak ditemukan
+        Hanya admin yg bisa mengakses halaman ini
     </div>
     <?php
 }
